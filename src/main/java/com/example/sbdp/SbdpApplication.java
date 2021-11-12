ce561dbe1b9ebd2d6e5bdb87cbe9b31d83fa5667
nam <namgd69@gmail.com>
2021-11-12 16:44:37 +0700

    alo alo

---
+++ src/main/java/com/example/sbdp/SbdpApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbdpApplication.class, args);
+	}
+
+}
